bb2bf0854ecf2ba43a4323bef54e200d0bcdf2cf
unknown <vaibhav.kaul2@gnmail.com>
2020-08-02 01:41:57 +0530

    Recipe-done with recipe page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import './App.css';
 
 import illustration_1 from './Assets/Images/Illustration1.png';
 import illustration_2 from './Assets/Images/Illustration2.png';
+import illustration_3 from './Assets/Images/Illustration3.png';
+import illustration_4 from './Assets/Images/Illustration4.png';
+import illustration_5 from './Assets/Images/Illustration5.png';
 import Container from './Components/Container';
 import FullRecipe from './Components/FullRecipe/FullRecipe';
 class App extends Component {
@@ -22,6 +25,9 @@ class App extends Component {
 					alt='Bg_Image'
 					className='bg_top_right-image'
 				/>
+				<img src={illustration_3} alt='Bg_Image' className='left_center' />
+				<img src={illustration_4} alt='Bg_Image' className='right_bottom' />
+				<img src={illustration_5} alt='Bg_Image' />
 				<img
 					src={illustration_1}
 					alt='Bg_Image'
